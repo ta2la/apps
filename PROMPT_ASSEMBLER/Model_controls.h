1ba98954c6f0742e57c9bce1f88918bd47338909
TallaP <petr.talla@dlubal.com>
2026-01-30 16:34:45 +0100

    [code_data]

--- PROMPT_ASSEMBLER/Model_controls.h
+++ PROMPT_ASSEMBLER/Model_controls.h
@@ -46,8 +46,9 @@ public:
             // ---- LOAD (projects)
             m_items.append({TextType,   "LOAD: ", ""});
             m_items.append({ButtonType, "prompt_assembler_data", "execute_script /home/pt/KADLUB/cvz/APPS/PROMPT_ASSEMBLER/prompt_assembler/data/prompt_assembler.t2l"});
-            m_items.append({ButtonType, "prompt_assembler", "execute_script D:/TIMEAXIS/2026/01/14_PROMPT_ASSEMBLER_LOAD/prompt_assembler.t2l"});
-            m_items.append({ButtonType, "DLUBAL",           "execute_script D:/TIMEAXIS/2026/01/14_PROMPT_ASSEMBLER_LOAD/dlubal.t2l"});
+            m_items.append({ButtonType, "prompt_assembler",       "execute_script D:/TIMEAXIS/2026/01/14_PROMPT_ASSEMBLER_LOAD/prompt_assembler.t2l"});
+            m_items.append({ButtonType, "DLUBAL",                 "execute_script D:/TIMEAXIS/2026/01/14_PROMPT_ASSEMBLER_LOAD/dlubal.t2l"});
+            m_items.append({ButtonType, "DLUBAL_MAT",             "execute_script D:/TIMEAXIS/2026/01/14_PROMPT_ASSEMBLER_LOAD/DLUBAL_MAT.t2l"});
 
             m_items.append({TextType,   "EXPORT: ", ""});
             m_items.append({ButtonType, "@view", "dir_merge_files --views"});
